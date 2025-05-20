0361dd7c186a88201625f70198b9b596b92ca0a6
Hans Bada <hans@groupewattetco.com>
2025-05-20 14:21:29 +0200

    centré

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Dashboard Salons", layout="wide")
+st.set_page_config(page_title="Dashboard Salons", layout="centered")
 
 # Charger les données
 df = pd.read_csv("salon_ok.csv")
